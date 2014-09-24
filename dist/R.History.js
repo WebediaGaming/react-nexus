8f2a70a58d39af12bb0ec6aa022f482fef3753d5
Elie Rotenberg <elie.rotenberg@gmail.com>
2014-09-25 01:58:15 +0200

    wc

--- dist/R.History.js
+++ dist/R.History.js
@@ -9,29 +9,28 @@ module.exports = function(R) {
         createPlugin: function createPlugin(storeName, dispatcherName) {
             return R.App.createPlugin({
                 displayName: "History",
+                updateStoreToHref: function updateStoreToHref(flux, href) {
+                    flux.getStore(storeName).set("/History/pathname", url.parse(href).pathname);
+                },
                 installInClient: function installInClient(flux, window) {
-                    var navigate = function navigate(href) {
-                        console.warn("navigate", href);
-                        flux.getStore(storeName).set("/History/pathname", url.parse(href).pathname);
-                    };
-                    flux.getDispatcher(dispatcherName).addActionListener("/History/navigate", function navigate(params) {
-                        return function(fn) {
+                    flux.getDispatcher(dispatcherName).addActionListener("/History/navigate", R.scope(function navigate(params) {
+                        return R.scope(function(fn) {
                             R.Debug.dev(function() {
                                 assert(params.pathname && _.isString(params.pathname), "/History/navigate: params.pathname: expecting String.");
                             });
                             var href = url.format(_.extend(url.parse(window.location.href), { pathname: params.pathname }));
                             window.history.pushState(null, null, href);
-                            navigate(href);
+                            this.updateStoreToHref(flux, href);
                             _.defer(fn);
-                        };
-                    });
-                    window.addEventListener("popstate", function() {
-                        navigate(window.location.href);
-                    });
-                    navigate(window.location.href);
+                        }, this);
+                    }, this));
+                    window.addEventListener("popstate", R.scope(function() {
+                        this.updateStoreToHref(flux, window.location.href);
+                    }, this));
+                    this.updateStoreToHref(flux, window.location.href);
                 },
                 installInServer: function installInServer(flux, req) {
-                    flux.getStore(storeName).set("/History/pathname", url.parse(req.url).pathname);
+                    this.updateStoreToHref(flux, url.parse(req.url).pathname);
                 },
             });
         },
@@ -52,9 +51,12 @@ module.exports = function(R) {
                         return regeneratorRuntime.wrap(function callee$3$0$(context$4$0) {
                             while (1) switch (context$4$0.prev = context$4$0.next) {
                             case 0:
-                                context$4$0.next = 2;
+                                console.warn("click", this.props.pathname);
+                                context$4$0.next = 3;
                                 return this.getFluxDispatcher(specs.dispatcherName).dispatch("/History/navigate", { pathname: this.props.pathname });
-                            case 2:
+                            case 3:
+                                console.warn("clicked", this.props.pathname);
+                            case 4:
                             case "end":
                                 return context$4$0.stop();
                             }
--- src/R.History.js
+++ src/R.History.js
@@ -9,29 +9,28 @@ module.exports = function(R) {
         createPlugin: function createPlugin(storeName, dispatcherName) {
             return R.App.createPlugin({
                 displayName: "History",
+                updateStoreToHref: function updateStoreToHref(flux, href) {
+                    flux.getStore(storeName).set("/History/pathname", url.parse(href).pathname);
+                },
                 installInClient: function installInClient(flux, window) {
-                    var navigate = function navigate(href) {
-                        console.warn("navigate", href);
-                        flux.getStore(storeName).set("/History/pathname", url.parse(href).pathname);
-                    };
-                    flux.getDispatcher(dispatcherName).addActionListener("/History/navigate", function navigate(params) {
-                        return function(fn) {
+                    flux.getDispatcher(dispatcherName).addActionListener("/History/navigate", R.scope(function navigate(params) {
+                        return R.scope(function(fn) {
                             R.Debug.dev(function() {
                                 assert(params.pathname && _.isString(params.pathname), "/History/navigate: params.pathname: expecting String.");
                             });
                             var href = url.format(_.extend(url.parse(window.location.href), { pathname: params.pathname }));
                             window.history.pushState(null, null, href);
-                            navigate(href);
+                            this.updateStoreToHref(flux, href);
                             _.defer(fn);
-                        };
-                    });
-                    window.addEventListener("popstate", function() {
-                        navigate(window.location.href);
-                    });
-                    navigate(window.location.href);
+                        }, this);
+                    }, this));
+                    window.addEventListener("popstate", R.scope(function() {
+                        this.updateStoreToHref(flux, window.location.href);
+                    }, this));
+                    this.updateStoreToHref(flux, window.location.href);
                 },
                 installInServer: function installInServer(flux, req) {
-                    flux.getStore(storeName).set("/History/pathname", url.parse(req.url).pathname);
+                    this.updateStoreToHref(flux, url.parse(req.url).pathname);
                 },
             });
         },
@@ -49,7 +48,9 @@ module.exports = function(R) {
                 handleClick: function handleClick(event) {
                     event.preventDefault();
                     co(function*() {
+                        console.warn("click", this.props.pathname);
                         yield this.getFluxDispatcher(specs.dispatcherName).dispatch("/History/navigate", { pathname: this.props.pathname });
+                        console.warn("clicked", this.props.pathname);
                     }).call(this);
                 },
                 render: function render() {
