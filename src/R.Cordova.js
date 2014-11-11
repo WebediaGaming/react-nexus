270812c21ea482b0f348e6b04b2450f21ea280fb
Elie Rotenberg <elie@rotenberg.io>
2014-11-11 12:56:40 +0100

    Updated Cordova to match the new Plugin definition syntax.

--- src/R.Cordova.js
+++ src/R.Cordova.js
@@ -1,20 +1,18 @@
 module.exports = function(R) {
   return (params) => {
     class Cordova extends R.App.Plugin {
-      constructor() {
+      constructor({ flux, window, req }) {
         super();
+        if(window) {
+          // Client-only init
+        }
+        else {
+          // Server-only init
+        }
       }
 
       getDisplayName() {
         return 'Cordova';
-      }
-
-      installInClient(flux, window) {
-
-      }
-
-      installInServer(flux, req) {
-
       }
     }
 
