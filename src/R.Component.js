f1ccff391ddd929b74ad9dda9ac7f287d1ebd541
Elie Rotenberg <elie@rotenberg.io>
2014-11-12 12:08:52 +0100

    Upgraded Root Pared Component with Root

--- src/R.Component.js
+++ src/R.Component.js
@@ -3,37 +3,16 @@ module.exports = function(R) {
   const should = R.should;
   const React = R.React;
 
-  /**
-  * <p>Defines a specific mixin</p>
-  * <p>This will allow to components to access the main methods of react-rails</p>
-  * <ul>
-  * <li> Component.getFlux => Provide Flux for the current component </li>
-  * </ul>
-  * @class R.Component
-  */
   const Component = {
     Mixin: {
-      /**
-      * <p>Refers to specifics mixins in order to manage Pure, Async, Animate and Flux methods</p>
-      * @property mixins
-      * @type {array.object}
-      */
+      _ComponentMixin: true,
+
       mixins: [R.Pure.Mixin, R.Async.Mixin, R.Animate.Mixin, R.Flux.Mixin],
-      /**
-      * <p>Defines context object for the current component<br />
-      * Allows all components using this mixin to have reference to R.Flux (Provides by the R.Root)</p>
-      * @property contextTypes
-      * @type {object} flux
-      */
+
       contextTypes: {
         flux: R.Flux.PropType,
       },
 
-      _ComponentMixinHasComponentMixin: true,
-      /** <p>Provide Flux for the current component</p>
-      * @method getFlux
-      * @return {object} this.context.flux The Flux of the App
-      */
       getFlux() {
         return this.context.flux;
       },
--- src/R.Root.js
+++ src/R.Root.js
@@ -1,61 +1,31 @@
 module.exports = function(R) {
-    var _ = require("lodash");
-    var assert = require("assert");
-    var React = R.React;
+  const _ = R._;
+  const should = R.should;
+  const React = R.React;
 
-    /**
-    * <p>Defines the specific mixin for the root component<br />
-    * This will allow components to access the main methods of react-rails</p>
-    * <ul>
-    * <li> Component.getFlux => Provide Flux for the current component </li>
-    * </ul>
-    * @class R.Root
-    */
-    var Root = {
-        Mixin: {
-            /**  
-            * <p>Refers to specifics mixins in order to manage Pure, Async, Animate and Flux methods</p>
-            * @property mixins
-            * @type {array.object}
-            */
-            mixins: [R.Pure.Mixin, R.Async.Mixin, R.Animate.Mixin, R.Flux.Mixin],
-            _AppMixinHasAppMixin: true,
+  const Root = {
+    Mixin: {
+      _RootMixin: true,
 
-            /** <p>Checking types</p>
-            * @property propTypes
-            * @type {object} flux
-            */
-            propTypes: {
-                flux: R.Flux.PropType,
-            },
+      mixins: [R.Pure.Mixin, R.Async.Mixin, R.Animate.Mixin, R.Flux.Mixin],
 
-            /** <p> Must be defined in order to use getChildContext </p>
-            * @property childContextTypes
-            * @type {object} flux 
-            */
-            childContextTypes: {
-                flux: R.Flux.PropType,
-            },
+      propTypes: {
+        flux: R.Flux.PropType,
+      },
 
-            /** <p>Provides all children access to the Flux of the App </p>
-            * @method getChildContext
-            * @return {object} flux The flux of current component
-            */
-            getChildContext: function getChildContext() {
-                return {
-                    flux: this.props.flux,
-                };
-            },
+      childContextTypes: {
+        flux: R.Flux.PropType,
+      },
 
-            /** <p>Provide Flux for the current component</p>
-            * @method getFlux
-            * @return {object} this.context.flux The Flux of the App
-            */
-            getFlux: function getFlux() {
-                return this.props.flux;
-            },
-        },
-    };
+      getChildContext() {
+        return { flux: this.props.flux };
+      },
 
-    return Root;
+      getFlux() {
+        return this.props.flux;
+      },
+    },
+  };
+
+  return Root;
 };
