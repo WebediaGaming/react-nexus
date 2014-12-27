3ad2bdc95eb64d626ee9bcbfc44160948631ebbc
Elie Rotenberg <elie@rotenberg.io>
2014-12-27 20:03:24 +0100

    Whiteflagged expr in jshintrc, reset package.json to 0.0.0.

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
 "use strict";
 
-require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;
+require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = process.env.NODE_ENV !== "production";var __PROD__ = !__DEV__;var __BROWSER__ = typeof window === "object";var __NODE__ = !__BROWSER__;
