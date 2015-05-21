c6d346499af6355b874b564eb5933ae2a927efcd
Clément Fiorio <clement.fiorio@gmail.com>
2015-05-21 12:13:29 +0200

    updated deps + updated eslint rules to v0.21.0

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ var __DEV__ = process.env.NODE_ENV !== 'production';
 var __PROD__ = !__DEV__;
 var __BROWSER__ = typeof window === 'object';
 var __NODE__ = !__BROWSER__;
-if (__DEV__) {
+if(__DEV__) {
   Promise.longStackTraces();
   Error.stackTraceLimit = Infinity;
 }
