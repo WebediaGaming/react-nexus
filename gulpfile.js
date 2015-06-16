89013ec20bfe422d4fe5909854282bc7bbd51573
Elie Rotenberg <elie@rotenberg.io>
2015-06-16 15:54:27 +0200

    Overhauled the general prupose of the directory. - __prelude is gone - dist is gone - tests are pre-wired - .babelrc is pre-wired - babel transform is now the responsibility of the final user (explained   in README).

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,24 @@
-var _ = require('lodash');
-var should = require('should');
-var Promise = (global || window).Promise = require('bluebird');
-var __DEV__ = (process.env.NODE_ENV !== 'production');
-var __PROD__ = !__DEV__;
-var __BROWSER__ = (typeof window === 'object');
-var __NODE__ = !__BROWSER__;
-if(__DEV__) {
-  Promise.longStackTraces();
-}
+require('babel/register')({
+  optional: ['runtime']
+});
 
-var del = require('del');
-var babel = require('gulp-babel');
 var eslint = require('gulp-eslint');
-var fs = Promise.promisifyAll(require('fs'));
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var plumber = require('gulp-plumber');
-var prepend = require('gulp-insert').prepend;
-var sourcemaps = require('gulp-sourcemaps');
-
-var readPrelude = fs.readFileAsync('./__prelude.js');
+var mocha = require('gulp-mocha');
 
 function lint() {
   return gulp.src('src/**/*.js')
-  .pipe(plumber())
-  .pipe(eslint())
-  .pipe(eslint.format());
-}
-
-function build() {
-  return readPrelude.then(function(prelude) {
-    return gulp.src('src/**/*.js')
     .pipe(plumber())
-    .pipe(prepend(prelude))
-    .pipe(babel({
-      modules: 'common',
-      optional: [
-        'runtime',
-        'es7.classProperties',
-        'es7.decorators',
-      ],
-    }))
-    .pipe(gulp.dest('dist'));
-  });
+    .pipe(eslint())
+    .pipe(eslint.format());
 }
 
-function clean() {
-  del(['dist']);
+function test() {
+  return gulp.src('src/__tests__/*.js')
+    .pipe(mocha());
 }
 
 gulp.task('lint', lint);
-gulp.task('clean', clean);
-gulp.task('build', ['lint', 'clean'], build);
-gulp.task('default', ['build']);
+gulp.task('test', test);
+gulp.task('default', ['lint', 'test']);
