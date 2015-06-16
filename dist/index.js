2ef22e8ab6157737e41bcece4cfcf89f17c79706
Clément Fiorio <clement.fiorio@gmail.com>
2015-06-16 12:27:12 +0200

    Revert "removed src and dist files, as Travis CI is deactivated now"
    This reverts commit d0fc9c12d57049c09e5015df625f024c67e317ba.

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var _ = require('lodash');
+var should = require('should');
+var Promise = (global || window).Promise = require('bluebird');
+var __DEV__ = process.env.NODE_ENV !== 'production';
+var __PROD__ = !__DEV__;
+var __BROWSER__ = typeof window === 'object';
+var __NODE__ = !__BROWSER__;
+if (__DEV__) {
+  Promise.longStackTraces();
+  Error.stackTraceLimit = Infinity;
+}
