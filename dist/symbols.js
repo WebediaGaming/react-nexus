16f621691edf9c9ad8b4e2fc70040508010bf514
Elie rotenberg <elie@rotenberg.io>
2015-09-30 16:34:52 +0200

    Added dist folder to .gitignore.

--- dist/symbols.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-exports['default'] = {
-  $nexus: '$nexusqVSgya2QQYRZ' };
-module.exports = exports['default'];
-// https://xkcd.com/221/
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInN5bWJvbHMuanN4Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztxQkFBZTtBQUNiLFFBQU0sRUFBRSxvQkFBb0IsRUFDN0IiLCJmaWxlIjoic3ltYm9scy5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IHtcclxuICAkbmV4dXM6ICckbmV4dXNxVlNneWEyUVFZUlonLCAvLyBodHRwczovL3hrY2QuY29tLzIyMS9cclxufTtcclxuIl0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
