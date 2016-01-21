f31793d3b03dfd2d917ead66929b1b4811f3b0d4
Elie Rotenberg <elie.rotenberg@gmail.com>
2016-01-21 17:05:04 +0100

    add actions example

--- lib/__tests__/fixtures/components/User.jsx
+++ lib/__tests__/fixtures/components/User.jsx
@@ -1,34 +1,41 @@
 import React from 'react';
 
 import Nexus from '../../..';
-const { stores, Store } = Nexus;
+const { actions, stores, Store } = Nexus;
 
-export default stores(({ userId }) => ({
-  userState: `/users/${userId}`,
-}))(class User extends React.Component {
-  static displayName = 'User';
+export default
+actions(({ userId }) => ({
+  deleteUser: `/users/${userId}/delete',`
+}))(
+  stores(({ userId }) => ({
+    userState: `/users/${userId}`,
+  }))(class User extends React.Component {
+    static displayName = 'User';
 
-  static propTypes = {
-    userState: Store.State.propType(React.PropTypes.shape({
-      userId: React.PropTypes.string,
-      nickname: React.PropTypes.string,
-    })),
-  };
+    static propTypes = {
+      userState: Store.State.propType(React.PropTypes.shape({
+        userId: React.PropTypes.string,
+        nickname: React.PropTypes.string,
+      })),
+      deleteUser: React.PropTypes.func,
+    };
 
-  render() {
-    const { userState } = this.props;
-    if(userState.isPending()) {
-      return <div className='User pending'>
-        {'Loading...'}
+    render() {
+      const { userState } = this.props;
+      if(userState.isPending()) {
+        return <div className='User pending'>
+          {'Loading...'}
+        </div>;
+      }
+      if(userState.isRejected()) {
+        return <div className='User rejected'>
+          {'Error: '}{userState.reason}
+        </div>;
+      }
+      return <div className='User'>
+        {userState.value.nickname}
+        <button onClick={() => this.props.deleteUser()}>{'X'}</button>
       </div>;
     }
-    if(userState.isRejected()) {
-      return <div className='User rejected'>
-        {'Error: '}{userState.reason}
-      </div>;
-    }
-    return <div className='User'>
-      {userState.value.nickname}
-    </div>;
-  }
-});
+  })
+);
