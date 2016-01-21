0f893bb26cb1dc731d39e24b4b09cff0210f062b
Elie rotenberg <elie@rotenberg.io>
2016-01-21 17:58:06 +0100

    added deps decorator

--- lib/__tests__/fixtures/components/User.jsx
+++ lib/__tests__/fixtures/components/User.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
 
 import Nexus from '../../..';
-const { actions, stores, Store } = Nexus;
+const { deps, Store } = Nexus;
 
-export default
-actions(({ userId }) => ({
-  deleteUser: `/users/${userId}/delete',`
-}))(
-  stores(({ userId }) => ({
+export default deps(({ userId }) => ({
+  actions: {
+    deleteUser: `/users/${userId}/delete`,
+  },
+  stores: {
     userState: `/users/${userId}`,
-  }))(class User extends React.Component {
-    static displayName = 'User';
+  },
+}))(class User extends React.Component {
+  static displayName = 'User';
 
-    static propTypes = {
-      userState: Store.State.propType(React.PropTypes.shape({
-        userId: React.PropTypes.string,
-        nickname: React.PropTypes.string,
-      })),
-      deleteUser: React.PropTypes.func,
-    };
+  static propTypes = {
+    deleteUser: React.PropTypes.func,
+    userState: Store.State.propType(React.PropTypes.shape({
+      userId: React.PropTypes.string,
+      nickname: React.PropTypes.string,
+    })),
+  };
 
-    render() {
-      const { userState } = this.props;
-      if(userState.isPending()) {
-        return <div className='User pending'>
-          {'Loading...'}
-        </div>;
-      }
-      if(userState.isRejected()) {
-        return <div className='User rejected'>
-          {'Error: '}{userState.reason}
-        </div>;
-      }
-      return <div className='User'>
-        {userState.value.nickname}
-        <button onClick={() => this.props.deleteUser()}>{'X'}</button>
+  render() {
+    const { userState } = this.props;
+    if(userState.isPending()) {
+      return <div className='User pending'>
+        {'Loading...'}
       </div>;
     }
-  })
-);
+    if(userState.isRejected()) {
+      return <div className='User rejected'>
+        {'Error: '}{userState.reason}
+      </div>;
+    }
+    return <div className='User'>
+      {userState.value.nickname}
+      <button onClick={() => this.props.deleteUser()}>{'X'}</button>
+    </div>;
+  }
+});
