76587b6ed77c9643709f1aff44eb4765bf13a3eb
Martin Bouladour <martin.bouladour@webedia-group.com>
2016-02-12 10:29:02 +0100

    Fix gulp lint task

--- config/gulp/tasks/lint.js
+++ config/gulp/tasks/lint.js
@@ -3,16 +3,18 @@ import gulp from 'gulp';
 import path from 'path';
 import plumber from 'gulp-plumber';
 
-const app = path.join(
+const jsFilesGlob = path.join(
   __dirname, // /config/gulp/tasks
   '..', // /config/gulp
   '..', // /config/
   '..', // /
   'lib', // /lib/
+  '**',
+  '*.js',
 );
 
 export default () =>
-  gulp.task('lint', () => gulp.src(app)
+  gulp.task('lint', () => gulp.src(jsFilesGlob)
     .pipe(plumber())
     .pipe(eslint())
     .pipe(eslint.format())
