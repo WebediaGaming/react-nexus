3359a01648ab011b7ee07d445f79aa996dd7c194
Hadrien Delaitre <hadrien.deuxlettres@gmail.com>
2016-02-17 08:50:23 +0100

    [TESTS] Setting up selenium's tests

--- lib/__tests__/fixtures/components/User.jsx
+++ lib/__tests__/fixtures/components/User.jsx
@@ -1,41 +1,77 @@
 import React from 'react';
 
-import Nexus from '../../..';
-const { deps, Store } = Nexus;
+import Nexus from '../../../';
+const { deps } = Nexus;
 
 export default deps(({ userId }) => ({
   actions: {
     deleteUser: `/users/${userId}/delete`,
-  },
-  stores: {
-    userState: `/users/${userId}`,
+    updateUser: `/users/${userId}/update`,
   },
 }))(class User extends React.Component {
   static displayName = 'User';
 
   static propTypes = {
     deleteUser: React.PropTypes.func,
-    userState: Store.State.propType(React.PropTypes.shape({
-      userId: React.PropTypes.string,
-      nickname: React.PropTypes.string,
-    })),
+    rank: React.PropTypes.string,
+    updateUser: React.PropTypes.func,
+    userId: React.PropTypes.number,
+    userName: React.PropTypes.string,
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputUserName: '',
+      inputRank: '',
+    };
+  }
+
+  updateInputUserName(ev) {
+    const inputUserName = ev.target.value;
+    this.setState({ inputUserName });
+  }
+
+  updateInputRank(ev) {
+    const inputRank = ev.target.value;
+    this.setState({ inputRank });
+  }
+
+  updateUser({ userName, rank }) {
+    const { updateUser } = this.props;
+    updateUser({ userName, rank });
+    this.setState({
+      inputUserName: '',
+      inputRank: '',
+    });
+  }
+
   render() {
-    const { userState } = this.props;
-    if(userState.isPending()) {
-      return <div className='User pending'>
-        {'Loading...'}
-      </div>;
-    }
-    if(userState.isRejected()) {
-      return <div className='User rejected'>
-        {'Error: '}{userState.reason}
-      </div>;
-    }
+    const { userId, userName, rank } = this.props;
+    const { inputUserName, inputRank } = this.state;
     return <div className='User'>
-      {userState.value.nickname}
+      <div className='UserId'>{`User #${userId}`}</div>
+      <div className='UserName'>{`User Name: ${userName}`}</div>
+      <div className='UserRank'>{`User Rank: ${rank}`}</div>
       <button onClick={() => this.props.deleteUser()}>{'X'}</button>
+      <div>
+        <input
+          id={`InputUserName-${userId}`}
+          onChange={(ev) => this.updateInputUserName(ev)}
+          placeholder={'Updated Name'}
+          value={inputUserName}
+        />
+        <input
+          id={`InputUserRank-${userId}`}
+          onChange={(ev) => this.updateInputRank(ev)}
+          placeholder={'Updated Rank'}
+          value={inputRank}
+        />
+        <button onClick={() => this.updateUser({
+          userName: inputUserName,
+          rank: inputRank,
+        })}>{'Update'}</button>
+      </div>
     </div>;
   }
 });
--- lib/__tests__/fixtures/components/Users.jsx
+++ lib/__tests__/fixtures/components/Users.jsx
@@ -1,35 +1,107 @@
 import React from 'react';
 
-import Nexus from '../../..';
-const { stores, Store } = Nexus;
+import Nexus from '../../../';
+const { deps, Store } = Nexus;
 
 import User from './User';
 
-export default stores(() => ({
-  usersState: '/users',
+export default deps(() => ({
+  actions: {
+    createUser: `/users/create`,
+    toggleUsersVisibility: `/ui/users/toggle/visibility`,
+  },
+  stores: {
+    users: '/users',
+    uiUsersVisibility: '/ui/users/visibility',
+  },
 }))(class Users extends React.Component {
   static displayName = 'Users';
 
   static propTypes = {
-    usersState: Store.State.propType(React.PropTypes.objectOf(React.PropTypes.string)),
+    createUser: React.PropTypes.func,
+    toggleUsersVisibility: React.PropTypes.func,
+    uiUsersVisibility: Store.State.propType(React.PropTypes.shape(React.PropTypes.boolean)),
+    users: Store.State.propType(React.PropTypes.shape({
+      userId: React.PropTypes.string,
+      nickname: React.PropTypes.string,
+    })),
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputUserName: '',
+      inputRank: '',
+    };
+  }
+
+  updateInputUserName(ev) {
+    const inputUserName = ev.target.value;
+    this.setState({ inputUserName });
+  }
+
+  updateInputRank(ev) {
+    const inputRank = ev.target.value;
+    this.setState({ inputRank });
+  }
+
+  createUser({ userName, rank }) {
+    const { createUser } = this.props;
+    createUser({ userName, rank });
+    this.setState({
+      inputUserName: '',
+      inputRank: '',
+    });
+  }
+
   render() {
-    const { usersState } = this.props;
-    if(usersState.isPending()) {
+    const { users, uiUsersVisibility } = this.props;
+    const { inputUserName, inputRank } = this.state;
+    if(users.isPending()) {
       return <div className='Users pending'>
         {'Loading...'}
       </div>;
     }
-    if(usersState.isRejected()) {
+    if(users.isRejected()) {
       return <div className='Users rejected'>
-        {'Error: '}{usersState.reason}
+        {'Error: '}{users.reason}
       </div>;
     }
-    return <ul className='Users'>{Object.keys(usersState.value).map((userId) =>
-      <li key={userId}>
-        <User userId={userId} />
-      </li>
-    )}</ul>;
+    const userList = users.value;
+    const userListVisibility = uiUsersVisibility.value;
+    return <div>
+      <div>
+        <button
+          id='UsersVisibility'
+          onClick={() => this.props.toggleUsersVisibility()}>
+          {'Show/Hide Users'}
+        </button>
+      </div>
+      { userListVisibility ? <ul className='Users'>{userList.map(({ userName, rank, userId }) =>
+        <li key={userId}>
+          <User rank={rank} userId={userId} userName={userName} />
+        </li>
+      )}</ul> : null }
+      <input
+        id='InputUserName'
+        onChange={(ev) => this.updateInputUserName(ev)}
+        placeholder={'User Name'}
+        value={inputUserName}
+      />
+      <input
+        id='InputUserRank'
+        onChange={(ev) => this.updateInputRank(ev)}
+        placeholder={'Rank'}
+        value={inputRank}
+      />
+      <button
+        id='CreateUser'
+        onClick={() => this.createUser({
+          userName: inputUserName,
+          rank: inputRank,
+        })}>
+        {'Create New User'}
+      </button>
+    </div>;
   }
 });
